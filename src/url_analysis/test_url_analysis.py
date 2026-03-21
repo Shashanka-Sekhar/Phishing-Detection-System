512a84fbaddfbf271ec68936d66479311dec0936
Shashanka Sekhar Dash <shashanksekhar04@gmail.com>
2026-03-21 18:53:55 +0530

    Added minor improvements to risk score analysis.

--- src/url_analysis/test_url_analysis.py
+++ src/url_analysis/test_url_analysis.py
@@ -1,10 +1,18 @@
 from extract_url import extract_urls
+from url_features import extract_url_features
+from domain_info import get_domain
+from url_risk_score import calculate_url_risk
 
-email = """
-Your account has been suspended.
-Click here: https://paypal-login-security.ru/login
-"""
+email = "Click here https://paypal-login-security.ru/login"
 
 urls = extract_urls(email)
 
-print(urls)
+for url in urls:
+    features = extract_url_features(url)
+    domain = get_domain(url)
+    risk = calculate_url_risk(features, domain)
+
+    print("URL:", url)
+    print("Domain:", domain)
+    print("Features:", features)
+    print("Risk Score:", risk)
--- src/url_analysis/url_risk_score.py
+++ src/url_analysis/url_risk_score.py
@@ -1,17 +1,23 @@
-def calculate_url_risk(features):
+def calculate_url_risk(features, domain):
 
     score = 0
 
-    if features["url_length"] > 50:
+    if features["url_length"] > 30:
         score += 0.2
 
-    if features["num_dots"] > 3:
+    if features["num_dots"] >= 2:
         score += 0.2
 
-    if features["num_hyphens"] > 2:
+    if features["num_hyphens"] >= 1:
         score += 0.2
 
     if features["has_ip"]:
-        score += 0.4
+        score += 0.3
+
+    suspicious_words = ["login", "verify", "secure", "account", "update"]
+
+    for word in suspicious_words:
+        if word in domain:
+            score += 0.1
 
     return min(score, 1)
