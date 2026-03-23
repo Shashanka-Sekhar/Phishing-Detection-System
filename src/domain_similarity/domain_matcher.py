6c0c66703c8d480a75b67ed0d713f69db80d0dff
Shashanka Sekhar Dash <shashanksekhar04@gmail.com>
2026-03-23 20:24:32 +0530

    Corrected some minor errors

--- src/domain_similarity/domain_matcher.py
+++ src/domain_similarity/domain_matcher.py
@@ -1,22 +1,38 @@
 trusted_domains = [
-"https://www.google.com/search?q=google.com", "amazon.com", "facebook.com", "apple.com", "microsoft.com", "paypal.com", "netflix.com", "yahoo.com", "youtube.com", "linkedin.com", "instagram.com", "twitter.com", "chase.com", "wellsfargo.com", "bankofamerica.com", "ebay.com", "walmart.com", "whatsapp.com", "wikipedia.org", "reddit.com", "adobe.com", "dropbox.com", "github.com", "roblox.com", "twitch.tv", "steamcommunity.com", "coinbase.com", "binance.com", "dhl.com", "fedex.com", "usps.com", "ups.com", "instagram.com", "tiktok.com", "pinterest.com", "spotify.com", "wordpress.com", "tumblr.com", "bbc.co.uk", "cnn.com", "nytimes.com", "zillow.com", "booking.com", "airbnb.com", "expedia.com", "uber.com", "lyft.com", "https://www.google.com/search?q=alibaba.com", "aliexpress.com", "etsy.com"
+"google.com", "amazon.com", "facebook.com", "apple.com", "microsoft.com", "paypal.com", "netflix.com", "yahoo.com", "youtube.com", "linkedin.com", "instagram.com", "twitter.com", "chase.com", "wellsfargo.com", "bankofamerica.com", "ebay.com", "walmart.com", "whatsapp.com", "wikipedia.org", "reddit.com", "adobe.com", "dropbox.com", "github.com", "roblox.com", "twitch.tv", "steamcommunity.com", "coinbase.com", "binance.com", "dhl.com", "fedex.com", "usps.com", "ups.com", "instagram.com", "tiktok.com", "pinterest.com", "spotify.com", "wordpress.com", "tumblr.com", "bbc.co.uk", "cnn.com", "nytimes.com", "zillow.com", "booking.com", "airbnb.com", "expedia.com", "uber.com", "lyft.com", "alibaba.com", "aliexpress.com", "etsy.com"
 ]
 from levenshtein_check import get_distance
+
+def extract_main_domain(domain):
+    return domain.split(".")[0]
+
 
 def find_closest_domain(input_domain):
 
     min_distance = float("inf")
     closest_domain = None
 
+    input_name = extract_main_domain(input_domain)
+
+    print("Input domain:", input_domain)
+    print("Extracted input:", input_name)
+
     for domain in trusted_domains:
 
-        distance = get_distance(input_domain, domain)
+        trusted_name = extract_main_domain(domain)
+
+        print("Comparing with:", trusted_name)
+
+        distance = get_distance(input_name, trusted_name)
+
+        print("Distance:", distance)
 
         if distance < min_distance:
             min_distance = distance
             closest_domain = domain
 
     return closest_domain, min_distance
+
 def detect_typosquatting(input_domain):
 
     closest_domain, distance = find_closest_domain(input_domain)
--- src/url_analysis/test_url_analysis.py
+++ src/url_analysis/test_url_analysis.py
@@ -4,13 +4,13 @@ from domain_info import get_domain
 from url_risk_score import calculate_url_risk
 from domain_similarity.domain_matcher import detect_typosquatting
 
-domain = get_domain(url)
-
-typo_result = detect_typosquatting(domain)
-
 email = "Click here https://paypal-login-security.ru/login"
 
 urls = extract_urls(email)
+
+domain = get_domain(urls)
+
+typo_result = detect_typosquatting(domain)
 
 for url in urls:
     features = extract_url_features(url)
